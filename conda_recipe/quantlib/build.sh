c033fbf525c23b08d11f27bde8eb4ac6c3367b89
domosute <domosute@users.noreply.github.com>
2018-05-01 16:42:08 -0500

    Update build.sh

--- conda_recipe/quantlib/build.sh
+++ conda_recipe/quantlib/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+set -x -e
+PREFIX= # Set prefix here
+INCLUDE_PATH=$PREFIX/include
+LIBRARY_PATH=$PREFIX/lib
+
 ./autogen.sh
-./configure
+./configure --prefix=$PREFIX --boost-include-dir=$INCLUDE_PATH --boost-lib-dir=$LIBRARY_PATH
 make -j 4
 make install
