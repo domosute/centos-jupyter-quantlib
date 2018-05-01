6ef9b7c722dcb5f2823a6e9cfa3ed68816644adc
domosute <domosute@users.noreply.github.com>
2018-05-01 16:47:03 -0500

    Update build.sh

--- conda_recipe/quantlib-python/build.sh
+++ conda_recipe/quantlib-python/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+PREFIX= # Set prefix here
+INCLUDE_PATH=$PREFIX/include
+LIBRARY_PATH=$PREFIX/lib
+
 ./autogen.sh
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --boost-include-dir=$INCLUDE_PATH --boost-lib-dir=$LIBRARY_PATH
 make -j 4 -C Python
 sudo make -C Python install
