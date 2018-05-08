528511dd8cb286c3d5e754b0edc973e63f19bd90
root <root@gig-dr-aco.gladius.corp>
2018-05-08 13:54:53 -0500

    added notebook folder

---
+++ notebook/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.certfile = u'.jupyter/jupyter.crt'
+c.NotebookApp.keyfile = u'.jupyter/jupyter.key'
+c.NotebookApp.ip = '*'
+c.NotebookApp.token = 'jupyter'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 9999
+
