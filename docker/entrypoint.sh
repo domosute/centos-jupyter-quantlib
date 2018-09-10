71eed9664a65ca370a22a513b4e3fbfa152a6b9c
domosute <domosute@users.noreply.github.com>
2018-09-10 14:31:15 -0500

    Create entrypoint.sh

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# generate a new self signed SSL certificate when none is provided in the volume
+if [ ! -f /etc/nginx/ssl/jupyter.key  ] || [ ! -f /etc/nginx/ssl/jupyter.crt ]
+then
+    openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc//ssl//certs/jupyter.key -out /etc/ssl//certs/jupyter.crt -subj "/C=US/ST=Chicago/L=Chicago/O=Self Signed/OU=IT Department/CN=gladius.corp"
+fi
+
+# Start Postfix at boot
+postfix start
+
+# Start Crontab UI at boot
+HOST=0.0.0.0 PORT=9000 crontab-ui
+
+# Run Jupyter
+runuser -l jupyter -c "/opt/conda/bin/jupyter lab --ip=*"
